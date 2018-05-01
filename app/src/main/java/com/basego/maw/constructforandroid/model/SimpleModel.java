c75a8781f84c3ea5d2ba488c726ccbc74cad7201
xiguangxingxiahuaxian <maw@neuqsoft.com>
2018-05-01 15:13:37 +0800

    2018-5-1 提交

--- app/src/main/java/com/basego/maw/constructforandroid/model/SimpleModel.java
+++ app/src/main/java/com/basego/maw/constructforandroid/model/SimpleModel.java
@@ -1,13 +1,10 @@
 package com.basego.maw.constructforandroid.model;
 
-import com.basego.maw.constructforandroid.api.AppConfig;
 import com.basego.maw.constructforandroid.api.RetrofitManager;
 import com.basego.maw.constructforandroid.api.SubscriptionManager;
 import com.basego.maw.constructforandroid.base.Subscribe;
 import com.basego.maw.constructforandroid.bean.Persion;
-import com.basego.maw.constructforandroid.bean.news.NewBeanDTO;
 
-import retrofit2.Retrofit;
 import rx.Observable;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
@@ -30,7 +27,7 @@ public class SimpleModel {
     * @version
     */
     public void getName (Subscribe<Persion>subscribe){
-        Observable<Persion> observable=RetrofitManager.getSingleton(0).getSimpleApiservice().getName();
+        Observable<Persion> observable=RetrofitManager.getSingleton().getSimpleApiservice().getName();
 
        Subscription subscription= observable.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
